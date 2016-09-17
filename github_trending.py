59e86246edf47959f241961e87255f9bc059c2af
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-09-18 03:22:29 +0400

    добалено описание функций

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,27 @@
+import datetime
+import json
+import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    now_date = datetime.date.today()
+    delta = datetime.timedelta(days=7)
+    r = requests.get(
+        'https://api.github.com/search/repositories?q=created:>%s&sort=stars'
+        % (now_date-delta)
+        )
+    trending_repositories = {}
+    trending_repositories = r.json()['items'][:top_size]
+    return trending_repositories
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    print ("Ссылка на репозиторий - количество открытых issue:")
+    repositories_amount = 20
+    repositories = get_trending_repositories(repositories_amount)
+    for i in range(repositories_amount):
+        print (
+            '%s - %s'
+            % (repositories[i]['html_url'],
+               repositories[i]['open_issues_count'])
+        )
